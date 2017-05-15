42da2afa38ba917db69fa3b171bb2aa92db2a223
Kevin McDonald <k3vinmcdonald@gmail.com>
2017-05-15 07:57:58 -0500

    Pull data from all major market hubs

--- crest/price_db.go
+++ crest/price_db.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/evepraisal/go-evepraisal"
@@ -40,8 +41,25 @@ var SpecialRegions = []struct {
 	stations []int64
 }{
 	{
+		// 10000002
 		name:     "jita",
 		stations: []int64{60003466, 60003760, 60003757, 60000361, 60000451, 60004423, 60002959, 60003460, 60003055, 60003469, 60000364, 60002953, 60000463, 60003463},
+	}, {
+		// 10000043
+		name:     "amarr",
+		stations: []int64{60008950, 60002569, 60008494},
+	}, {
+		// 10000032
+		name:     "dodixie",
+		stations: []int64{60011866, 60001867},
+	}, {
+		// 10000027
+		name:     "rens",
+		stations: []int64{60004588, 60005725, 60004594, 60012727, 60012721, 60012724, 60009106},
+	}, {
+		// 10000042
+		name:     "hek",
+		stations: []int64{60005236, 60004516, 60015140, 60005686, 60011287, 60005236},
 	},
 }
 
@@ -70,8 +88,9 @@ func NewPriceDB(cache evepraisal.CacheDB, baseURL string) (evepraisal.PriceDB, e
 
 	go func() {
 		for {
+			start := time.Now()
 			priceDB.runOnce()
-			time.Sleep(5 * time.Minute)
+			time.Sleep((5 * time.Minute) - time.Since(start))
 		}
 	}()
 
@@ -104,7 +123,7 @@ type MarketOrderResponse struct {
 
 func (p *PriceDB) runOnce() {
 	log.Println("Fetch market data")
-	priceMap, err := p.FetchMarketData(p.client, p.baseURL, 10000002)
+	priceMap, err := p.FetchMarketData(p.client, p.baseURL, []int{10000002, 10000042, 10000027, 10000032, 10000043})
 	if err != nil {
 		log.Println("ERROR: fetching market data: ", err)
 		return
@@ -132,37 +151,50 @@ func (p *PriceDB) freshPriceMap() map[string]map[int64]evepraisal.Prices {
 	return priceMap
 }
 
-func (p *PriceDB) FetchMarketData(client *http.Client, baseURL string, regionID int) (map[string]map[int64]evepraisal.Prices, error) {
+func (p *PriceDB) FetchMarketData(client *http.Client, baseURL string, regionIDs []int) (map[string]map[int64]evepraisal.Prices, error) {
 	allOrdersByType := make(map[int64][]MarketOrder)
+
+	l := &sync.Mutex{}
 	requestAndProcess := func(url string) (error, string) {
 		var r MarketOrderResponse
 		err := fetchURL(client, url, &r)
 		if err != nil {
 			return err, ""
 		}
+
+		l.Lock()
 		for _, order := range r.Items {
 			allOrdersByType[order.Type] = append(allOrdersByType[order.Type], order)
 		}
+		l.Unlock()
 		return nil, r.Next.HREF
 	}
 
-	url := fmt.Sprintf("%s/market/%d/orders/all/", baseURL, regionID)
-	for {
-		err, next := requestAndProcess(url)
-		if err != nil {
-			return nil, err
-		}
+	wg := &sync.WaitGroup{}
+	for _, regionID := range regionIDs {
+		wg.Add(1)
+		go func(regionID int) {
+			defer wg.Done()
+			url := fmt.Sprintf("%s/market/%d/orders/all/", baseURL, regionID)
+			for {
+				err, next := requestAndProcess(url)
+				if err != nil {
+					log.Println("WARNING: Failed to fetch market orders", err)
+				}
 
-		if next == "" {
-			break
-		} else {
-			url = next
-		}
+				if next == "" {
+					break
+				} else {
+					url = next
+				}
+			}
+		}(regionID)
 	}
+	wg.Wait()
 
+	log.Println("Performing aggregates on order data")
 	// Calculate aggregates that we care about:
 	newPriceMap := p.freshPriceMap()
-
 	for k, orders := range allOrdersByType {
 		for _, region := range SpecialRegions {
 			filteredOrders := make([]MarketOrder, 0)
@@ -185,6 +217,8 @@ func (p *PriceDB) FetchMarketData(client *http.Client, baseURL string, regionID 
 
 		newPriceMap["universe"][k] = getPriceAggregatesForOrders(orders)
 	}
+
+	log.Println("Finished performing aggregates on order data")
 
 	return newPriceMap, nil
 }
