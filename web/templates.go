47e47520c9d25f89470c0a548de29b18ea2ca9f0
Kevin McDonald <k3vinmcdonald@gmail.com>
2017-11-20 19:26:59 -0600

    Show number of items in latest and history pages

--- web/templates.go
+++ web/templates.go
@@ -17,8 +17,9 @@ import (
 
 var templateFuncs = template.FuncMap{
 	"humanizeVolume":  humanizeVolume,
-	"commaf":          humanizeCommaf,
 	"comma":           humanize.Comma,
+	"commaf":          humanizeCommaf,
+	"commai":          func(i int) string { return humanize.Comma(int64(i)) },
 	"prettybignumber": HumanLargeNumber,
 	"relativetime":    humanize.Time,
 	"timefmt":         func(t time.Time) string { return t.Format("2006-01-02 15:04:05") },
