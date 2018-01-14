58445930c28afece664045e40a51a73e21f730dc
Kevin McDonald <k3vinmcdonald@gmail.com>
2018-01-14 08:34:06 -0600

    Backup adjustments

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,16 +2,15 @@
 set -e
 
 export RCLONE_CONFIG="/etc/evepraisal/rclone.conf"
-echo "$(date) - restarting service to get nmap buffer"
-/usr/sbin/service evepraisal restart
 
 echo "$(date) - starting backup of supporting files"
 mkdir -p /usr/local/evepraisal/backups/
+rm /usr/local/evepraisal/backups/appraisals.gz
 rclone copy /usr/local/evepraisal/db/certs gdrive:backups/evepraisal/usr/local/evepraisal/db/certs
 rclone copy /etc/evepraisal/evepraisal.toml gdrive:backups/evepraisal/etc/evepraisal/
 
 echo "$(date) - starting backup of appraisal database"
-curl 127.0.0.1:8090/backup/appraisals | bzip2 --stdout > /usr/local/evepraisal/backups/appraisals.bak.bz2
+curl 127.0.0.1:8090/backup-to-file/appraisals
 echo "$(date) - uploading backup of appraisal database"
-rclone move /usr/local/evepraisal/backups/appraisals.bz2 gdrive:backups/evepraisal/usr/local/evepraisal/db/
+rclone move /usr/local/evepraisal/backups/appraisals.gz gdrive:backups/evepraisal/usr/local/evepraisal/db/
 echo "$(date) - finished backup"
