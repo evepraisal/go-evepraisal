f3aa8c2a0adabaca47d35a6cbf7537d81a62f390
Kevin McDonald <k3vinmcdonald@gmail.com>
2018-08-27 17:15:08 -0500

    Add explicit support for 'Copy material information' in industry panel

--- parsers/industry.go
+++ parsers/industry.go
@@ -43,9 +43,64 @@ var reIndustryBlueprints = regexp.MustCompile(strings.Join([]string{
 	`(?:\t([\S ]*))`,                      // Group
 }, ""))
 
+var reIndustryMaterials = regexp.MustCompile(strings.Join([]string{
+	`^([\S\ ]+)`,                   // Name
+	`\t([` + bigNumberRegex + `*)`, // Required
+	`\t([` + bigNumberRegex + `*)`, // Available
+	`\t([` + bigNumberRegex + `*)`, // Est. Unit price
+	`\t([` + bigNumberRegex + `*)`, // typeID
+}, ""))
+
+var industryHeaders = map[string]bool{
+	"Components				": true,
+	"Minerals				": true,
+	"Planetary materials				": true,
+	"Items				": true,
+	"Datacores				": true,
+	"Optional items				": true,
+	"No item selected				": true,
+	`Item	Required	Available	Est. Unit price	typeID`: true,
+}
+
+func hasMaterialHeaders(input Input) ([]int, bool) {
+	var hasHeader = false
+	var removeLines = make([]int, 0)
+	for idx, val := range input {
+		_, isHeader := industryHeaders[val]
+		if isHeader {
+			removeLines = append(removeLines, idx)
+			hasHeader = true
+		}
+		if val == "" {
+			removeLines = append(removeLines, idx)
+		}
+	}
+	return removeLines, hasHeader
+}
+
 // ParseIndustry parses industry window text
 func ParseIndustry(input Input) (ParserResult, Input) {
 	industry := &Industry{}
+	removeLines, isMaterials := hasMaterialHeaders(input)
+	if isMaterials {
+		for _, line := range removeLines {
+			industry.lines = append(industry.lines, line)
+			delete(input, line)
+		}
+
+		matches, rest := regexParseLines(reIndustryMaterials, input)
+		industry.lines = append(industry.lines, regexMatchedLines(matches)...)
+		for _, match := range matches {
+			industry.Items = append(industry.Items, IndustryItem{Name: match[1], Quantity: ToInt(match[2])})
+		}
+
+		sort.Slice(industry.Items, func(i, j int) bool {
+			return fmt.Sprintf("%v", industry.Items[i]) < fmt.Sprintf("%v", industry.Items[j])
+		})
+		sort.Ints(industry.lines)
+		return industry, rest
+	}
+
 	matches, rest := regexParseLines(reIndustry, input)
 	industry.lines = append(industry.lines, regexMatchedLines(matches)...)
 
@@ -70,7 +125,6 @@ func ParseIndustry(input Input) (ParserResult, Input) {
 		}
 		matchgroup[IndustryItem{Name: match[2], BPC: isBPC, BPCRuns: runCount}] += count
 	}
-
 	// add items w/totals
 	for item, quantity := range matchgroup {
 		item.Quantity = quantity
--- parsers/industry_test.go
+++ parsers/industry_test.go
@@ -43,5 +43,54 @@ Victorieux Luxury Yacht Blueprint	0	0	1	Cruiser
 			lines: []int{0, 1, 2, 3}},
 		Input{},
 		true,
+	}, {
+		"Copy Material Information",
+		`Components				
+Item	Required	Available	Est. Unit price	typeID
+Plasma Thruster	30	0	75199.17	11530
+Fernite Carbide Composite Armor Plate	294	0	14872.75	11542
+
+Minerals				
+Item	Required	Available	Est. Unit price	typeID
+Morphite	38	0	10558.3	11399
+
+Planetary materials				
+Item	Required	Available	Est. Unit price	typeID
+Construction Blocks	30	0	11551.94	3828
+
+Items				
+Item	Required	Available	Est. Unit price	typeID
+Breacher	1	0	393441.48	598
+R.A.M.- Starship Tech	3	0	314.02	11478`,
+		&Industry{
+			Items: []IndustryItem{
+				{Name: "Breacher", Quantity: 1},
+				{Name: "Construction Blocks", Quantity: 30},
+				{Name: "Fernite Carbide Composite Armor Plate", Quantity: 294},
+				{Name: "Morphite", Quantity: 38},
+				{Name: "Plasma Thruster", Quantity: 30},
+				{Name: "R.A.M.- Starship Tech", Quantity: 3},
+			},
+			lines: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}},
+		Input{},
+		true,
+	}, {
+		"Copy Material Information (datacores)",
+		`Datacores				
+Item	Required	Available	Est. Unit price	typeID
+Datacore - Mechanical Engineering	60	0	43694.05	20424
+Datacore - Minmatar Starship Engineering	60	0	93418.68	20172
+
+Optional items				
+Item	Required	Available	Est. Unit price	typeID
+No item selected				`,
+		&Industry{
+			Items: []IndustryItem{
+				{Name: "Datacore - Mechanical Engineering", Quantity: 60},
+				{Name: "Datacore - Minmatar Starship Engineering", Quantity: 60},
+			},
+			lines: []int{0, 1, 2, 3, 4, 5, 6, 7}},
+		Input{},
+		true,
 	},
 }
